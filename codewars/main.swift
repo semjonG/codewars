8d2a6e4ed3f99279fee36ae43f3f9b19cc8da949
SemjonG <simon.gerasimov@gmail.com>
2022-06-12 22:27:07 +0300

    new tasks done

--- codewars/main.swift
+++ codewars/main.swift
@@ -168,7 +168,7 @@ import Foundation
 
 
 
-//MARK: 10. Task. -  done
+//MARK: 10. Task. - done
 //Complete the square sum function so that it squares each number passed into it and then sums the results together.
 //For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
@@ -176,3 +176,38 @@ import Foundation
 //    return vals.map { $0 * $0 }.reduce(0, +)
 //}
 //print(squareSum([1, 2, 2]))
+
+
+
+//MARK: 11. Task. - done
+//Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+//
+//Numerical Score    Letter Grade
+//90 <= score <= 100    'A'
+//80 <= score < 90    'B'
+//70 <= score < 80    'C'
+//60 <= score < 70    'D'
+//0 <= score < 60    'F'
+//Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+//func getGrade(_ s1: Int, _ s2: Int, _ s3: Int) -> String {
+//    let averageNumber = (s1 + s2 + s3) / 3
+//
+//    switch averageNumber {
+//    case 90..<101:
+//        return "A"
+//    case 80..<90:
+//        return "B"
+//    case 70..<80:
+//        return "C"
+//    case 60..<70:
+//        return "D"
+//    case 0..<60:
+//        return "F"
+//    default:
+//        print("Error: use consistent values")
+//    }
+//    return ""
+//}
+//
+//print(getGrade(60, 80, 90))
