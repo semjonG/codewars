434e34c36b25d989ce0d3a2bfc369578d6cdb614
SemjonG <simon.gerasimov@gmail.com>
2022-08-01 18:37:06 +0300

    55 tasks done

--- codewars/main.swift
+++ codewars/main.swift
@@ -815,3 +815,30 @@ import Foundation
 //      return  "none"
 //  }
 //}
+
+
+//MARK: 53. Task. - done
+//var laLigaGoals = 43
+//var championLeagueGoals = 10
+//var copaDelReyGoals = 5
+//
+//var totalGoals = laLigaGoals + championLeagueGoals + copaDelReyGoals
+
+
+
+//MARK: 54. Task. - done
+//In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+//func move(_ p: Int, _ r: Int) -> Int {
+//    return (r*2)+p
+//}
+
+
+//MARK: 55. Task. - done
+//This function should test if the factor is a factor of base.
+//Return true if it is a factor or false if it is not.
+
+//func checkForFactor(_ base: Int, _ factor: Int) -> Bool {
+//    base % factor == 0 ? true : false
+//    // return base % factor == 0
+//}
